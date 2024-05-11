50402d4f436800f2a1581d094ee5200d781c34db
Priyanshu727 <priyanshumishra7272@gmail.com>
2024-05-11 16:22:55 +0530

    Admin Panel

--- index.js
+++ index.js
@@ -1,30 +1,91 @@
-const express = require('express')
-// const path = require('path')
-const port = 8081;
+const express = require("express");
+
+const port = 8000;
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine","ejs");
+const path = require("path")
 
-// app.use(express.static(path.join(__dirname+'/public')))
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    return res.render('layout');
+const middleware = (req,res,next)=>{
+    console.log("hello");
+    let age = req.query.age;
+    if(age >= 18){
+        return next;
+    }
+  return res.redirect('/')
+}
+//app.use(middleware);
+app.use(express.static(path.join(__dirname,"/public")));app.get('/', (req, res) => {
+    return res.render('./layout');
 })
-
+app.get('/authentication-login', (req, res) => {
+    return res.render('./pages/authentication-login')
+})
+app.get('/authentication-register', (req, res) => {
+    return res.render('./pages/authentication-register')
+})
 app.get('/charts', (req, res) => {
-    return res.render('./pages/charts');
+    return res.render('./pages/charts')
 })
-
+app.get('/error-403', (req, res) => {
+    return res.render('./pages/error-403')
+})
+app.get('/error-404', (req, res) => {
+    return res.render('./pages/error-404')
+})
+app.get('/error-405', (req, res) => {
+    return res.render('./pages/error-405')
+})
+app.get('/error-500', (req, res) => {
+    return res.render('./pages/error-500')
+})
+app.get('/form-basic', (req, res) => {
+    return res.render('./pages/form-basic')
+})
+app.get('/form-wizard', (req, res) => {
+    return res.render('./pages/form-wizard')
+})
+app.get('/grid', (req, res) => {
+    return res.render('./pages/grid')
+})
+app.get('/icon-fontawesome', (req, res) => {
+    return res.render('./pages/icon-fontawesome')
+})
+app.get('/icon-material', (req, res) => {
+    return res.render('./pages/icon-material')
+})
+app.get('/index2', (req, res) => {
+    return res.render('./pages/index2')
+})
+app.get('/pages-buttons', (req, res) => {
+    return res.render('./pages/pages-buttons')
+})
+app.get('/pages-calendar', (req, res) => {
+    return res.render('./pages/pages-calendar')
+})
+app.get('/pages-chat', (req, res) => {
+    return res.render('./pages/pages-chat')
+})
+app.get('/pages-elements', (req, res) => {
+    return res.render('./pages/pages-elements')
+})
+app.get('/pages-gallery', (req, res) => {
+    return res.render('./pages/pages-gallery')
+})
+app.get('/pages-invoice', (req, res) => {
+    return res.render('./pages/pages-invoice')
+})
+app.get('/tables', (req, res) => {
+    return res.render('./pages/tables')
+})
 app.get('/widgets', (req, res) => {
-    return res.render('./pages/widgets');
+    return res.render('./pages/widgets')
 })
-
-app.listen(port, (err) => {
-    if (err) {
-        console.log("Server in not start in port");
+app.listen(port,(err)=>{
+    if(err){
+        console.log("Server Not Started");
         return false;
     }
-    console.log("Server start http://localhost:" + port);
-})
+    console.log("http://localhost:"+port)
+});
